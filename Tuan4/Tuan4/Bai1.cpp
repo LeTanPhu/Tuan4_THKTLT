7855f1eec187da00ebf4b76509145d657df80266
LeTanPhu <phule13112004@gmail.com>
2024-08-06 21:42:51 +0700

    Edit Bai1.cpp

--- Tuan4/Tuan4/Bai1.cpp
+++ Tuan4/Tuan4/Bai1.cpp
@@ -1,33 +1,29 @@
-#include<conio.h>
 #include<stdio.h>
+#include<conio.h>
+#include<string.h>
 #include<stdlib.h>
+#include<math.h>
 int TongDQ(int n) {
 	if (n == 1) return 1;
-	else return n+TongDQ(n - 1);
+	else return n + TongDQ(n - 1);
 }
-int TongKhuDQ(int n) {
-	int tong = 0;
-	for (int i = 0; i <= n; i++)
-	{
-		tong += i;
-	}
-	return tong;
+float TongDQPS(int n) {
+	if (n == 1) return 0.5; else
+		return (float)n / (n + 1) + TongDQPS(n - 1);
 }
-float TongPSKhuDQ(int n) {
-	float tong1 = 0;
-	for (float i = 1; i <=n; i++)
-	{
-		float tinh = i / (i + 1);
-		tong1 = tong1 + tinh;
-	}
-	return tong1;
+float TongDQS3(int n) {
+	if (n == 0) return 1; else return (float)1 / (2*n + 1) + TongDQS3(n - 1);
+}
+int TongDQS4(int n) {
+	if (n == 1) return 2; else return (n* (n + 1)) + TongDQS4(n - 1);
 }
 int main() {
 	int n;
 	printf("Nhap vao n:");
 	scanf("%d", &n);
 	printf("\nKet qua cua tong (DQ):%d", TongDQ(n));
-	printf("\nKet qua cua tong (Khu DQ):%d", TongKhuDQ(n));
-	printf("\nKet qua cua tong phan so:%.2f", TongPSKhuDQ(n));
+	printf("\nKet qua cua tong PS:%.2f", TongDQPS(n));
+	printf("\nKet qua cua tong 1/2n+1:%.2f", TongDQS3(n));
+	printf("\nKet qua cua tong n*(n+1):%d", TongDQS4(n));
 	getch();
 }
